e9bfa98ffc9a4d54285ce468992358ab9eb8af5c
mts0629 <soratobi93@gmail.com>
2022-06-26 13:47:40 +0900

    Comment out old test temporally

--- test/runner/all_tests.c
+++ test/runner/all_tests.c
@@ -11,27 +11,27 @@ static void RunAllTests(void)
 
     RUN_TEST_GROUP(scnn_layer);
 
-    RUN_TEST_GROUP(data);
+    //RUN_TEST_GROUP(data);
 
-    RUN_TEST_GROUP(util);
+    //RUN_TEST_GROUP(util);
 
-    RUN_TEST_GROUP(random);
+    //RUN_TEST_GROUP(random);
 
-    RUN_TEST_GROUP(mat);
+    //RUN_TEST_GROUP(mat);
 
-    RUN_TEST_GROUP(layer);
+    //RUN_TEST_GROUP(layer);
 
-    RUN_TEST_GROUP(fc);
+    //RUN_TEST_GROUP(fc);
 
-    RUN_TEST_GROUP(sigmoid);
+    //RUN_TEST_GROUP(sigmoid);
 
-    RUN_TEST_GROUP(softmax);
+    //RUN_TEST_GROUP(softmax);
 
-    RUN_TEST_GROUP(net);
+    //RUN_TEST_GROUP(net);
 
-    RUN_TEST_GROUP(loss);
+    //RUN_TEST_GROUP(loss);
 
-    RUN_TEST_GROUP(trainer);
+    //RUN_TEST_GROUP(trainer);
 }
 
 int main(int argc, const char *argv[])
