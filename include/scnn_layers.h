b456f9a2fcae2cf34e2264bc238e6bcda66b7fde
mts0629 <soratobi93@gmail.com>
2022-09-19 10:26:46 +0900

    Add header for all layers

---
+++ include/scnn_layers.h
@@ -0,0 +1,13 @@
+/**
+ * @file scnn_layers.h
+ * @brief Layer definitions
+ * 
+ */
+#ifndef SCNN_LAYERS_H
+#define SCNN_LAYERS_H
+
+#include "scnn_fc.h"
+#include "scnn_sigmoid.h"
+#include "scnn_softmax.h"
+
+#endif // SCNN_LAYERS_H
