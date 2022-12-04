001240f363d6a0d28bd127ff11ff5bf66ff4e5c5
mts0629 <soratobi93@gmail.com>
2022-12-04 20:56:34 +0900

    Validate layers on initialize network

--- test/runner/test_scnn_net_runner.c
+++ test/runner/test_scnn_net_runner.c
@@ -23,6 +23,11 @@ TEST_GROUP_RUNNER(scnn_net)
     RUN_TEST_CASE(scnn_net, init_2layers);
     RUN_TEST_CASE(scnn_net, init_3layers);
 
+    RUN_TEST_CASE(scnn_net, cannot_init_if_net_is_NULL);
+    RUN_TEST_CASE(scnn_net, cannot_init_if_size_is_0);
+    RUN_TEST_CASE(scnn_net, cannot_init_if_in_shape_is_invalid);
+    RUN_TEST_CASE(scnn_net, cannot_init_if_contains_invalid_layer);
+
     //RUN_TEST_CASE(scnn_net, forward);
     //RUN_TEST_CASE(scnn_net, forward_net_is_NULL);
     //RUN_TEST_CASE(scnn_net, forward_x_is_NULL);
