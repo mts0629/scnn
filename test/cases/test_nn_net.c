442270702a66c9fab88259d545c62a2563fe997f
mts0629 <soratobi93@gmail.com>
2024-02-17 16:57:01 +0900

    Verify updating of the network

--- test/cases/test_nn_net.c
+++ test/cases/test_nn_net.c
@@ -5,6 +5,8 @@
  */
 #include "nn_net.h"
 
+#include <string.h>
+
 #include "activation.h"
 #include "blas.h"
 #include "loss.h"
@@ -15,9 +17,9 @@
 NnNet *net;
 
 NnLayerParams dummy_layer_params[] = {
-    { .batch_size = 1, .in = 3 * 28 * 28, .out = 100 },
+    { .batch_size = 1, .in = 2, .out = 100 },
     { .out = 10 },
-    { .out = 2 }
+    { .out = 1 }
 };
 
 float dummy_x[3 * 28 * 28];
@@ -136,16 +138,16 @@ void test_init_fail_if_net_size_is_0(void) {
     nn_net_free(&net);
 }
 
-static void fill_with_val(float *array, const float value, const size_t size) {
+static void fill_with_value(float *array, const float value, const size_t size) {
     for (int i = 0; i < size; i++) {
-        array[i] = 0;
+        array[i] = value;
     }
 }
 
-static void net_fill_with_val(NnNet *net, const float value) {
+static void net_fill_with_value(NnNet *net, const float value) {
     for (int i = 0; i < net->size; i++) {
-        fill_with_val(net->layers[i].w, value, (net->layers[i].in * net->layers[i].out));
-        fill_with_val(net->layers[i].b, value, net->layers[i].out);
+        fill_with_value(net->layers[i].w, value, (net->layers[i].in * net->layers[i].out));
+        fill_with_value(net->layers[i].b, value, net->layers[i].out);
     }
 }
 
@@ -156,7 +158,7 @@ void test_forward_1layer(void) {
 
     nn_net_init(net);
 
-    net_fill_with_val(net, 0);
+    net_fill_with_value(net, 0);
 
     TEST_ASSERT_EQUAL_PTR(nn_net_layers(net)[0].z, nn_net_forward(net, dummy_x));
 
@@ -172,7 +174,7 @@ void test_forward_3layer(void) {
 
     nn_net_init(net);
 
-    net_fill_with_val(net, 0);
+    net_fill_with_value(net, 0);
 
     TEST_ASSERT_EQUAL_PTR(nn_net_layers(net)[2].z, nn_net_forward(net, dummy_x));
 
@@ -237,15 +239,64 @@ void test_backward_fail_if_dy_is_NULL(void) {
     nn_net_free(&net);
 }
 
+static void verify_params_are_different(const float *a1, const float *a2, const size_t size) {
+    for (int i = 0; i < size; i++) {
+        TEST_ASSERT_TRUE(a1[i] != a2[i]);
+    }
+}
+
 void test_update(void) {
     net = nn_net_alloc();
     nn_net_append(net, dummy_layer_params[0]);
     nn_net_append(net, dummy_layer_params[1]);
+    nn_net_append(net, dummy_layer_params[2]);
 
     nn_net_init(net);
 
-    // TODO: meaningful validation
-    nn_net_update(net, 0.01);
+    net_fill_with_value(net, 0.1);
+
+    float x[] = { 0.1, 0.5 };
+
+    nn_net_forward(net, x);
+
+    float dy[] = { 0.5 };
+    nn_net_backward(net, dy);
+
+    // Store the weights/biases before update
+    float prev_w1[100 * 2];
+    float prev_b1[100];
+    float prev_w2[10 * 100];
+    float prev_b2[10];
+    float prev_w3[1 * 10];
+    float prev_b3[1];
+
+    memcpy(prev_w1, net->layers[0].w, sizeof(prev_w1));
+    memcpy(prev_b1, net->layers[0].b, sizeof(prev_b1));
+    memcpy(prev_w2, net->layers[1].w, sizeof(prev_w2));
+    memcpy(prev_b2, net->layers[1].b, sizeof(prev_b2));
+    memcpy(prev_w3, net->layers[2].w, sizeof(prev_w3));
+    memcpy(prev_b3, net->layers[2].b, sizeof(prev_b3));
+
+    nn_net_update(net, 0.1);
+
+    verify_params_are_different(
+        prev_w1, net->layers[0].w, (net->layers[0].in * net->layers[0].out)
+    );
+    verify_params_are_different(
+        prev_b1, net->layers[0].b, net->layers[0].out
+    );
+    verify_params_are_different(
+        prev_w2, net->layers[1].w, (net->layers[1].in * net->layers[1].out)
+    );
+    verify_params_are_different(
+        prev_b2, net->layers[1].b, net->layers[1].out
+    );
+    verify_params_are_different(
+        prev_w3, net->layers[2].w, (net->layers[2].in * net->layers[2].out)
+    );
+    verify_params_are_different(
+        prev_b3, net->layers[2].b, net->layers[2].out
+    );
 
     nn_net_free(&net);
 }
