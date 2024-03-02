62d9dcde1258656bf4974319a8410df6d5df7a76
mts0629 <soratobi93@gmail.com>
2024-03-02 23:58:19 +0900

    Update XOR example

--- example/xor/xor.c
+++ example/xor/xor.c
@@ -5,13 +5,15 @@
 #include "loss.h"
 #include "nn_trainer.h"
 
-void init_rand(float *array, const size_t size) {
+#define BATCH_SIZE 4
+
+static void init_rand(float *array, const size_t size) {
     for (size_t i = 0; i < size; i++) {
         array[i] = 2 * ((float)rand() / RAND_MAX) - 1;
     }
 }
 
-void net_randomize(NnNet *net) {
+static void net_randomize(NnNet *net) {
     srand(time(NULL));
 
     for (int i = 0; i < net->size; i++) {
@@ -21,17 +23,35 @@ void net_randomize(NnNet *net) {
     }
 }
 
+static int get_class(const float value) {
+    return (value > 0.5f ? 1 : 0);
+}
+
+static float get_accuracy(const float *y, const float *t, const size_t size) {
+    int correct = 0;
+
+    for (size_t i = 0; i < size; i++) {
+        if (get_class(y[i]) == (int)t[i]) {
+            correct++;
+        }
+    }
+
+    return (float)correct / size;
+}
+
 int main(void) {
     NnNet *net = nn_net_alloc();
-    nn_net_append(net, (NnLayerParams){ .batch_size=4, .in=2, .out=100 });
+    nn_net_append(net, (NnLayerParams){ .batch_size=BATCH_SIZE, .in=2, .out=100 });
     nn_net_append(net, (NnLayerParams){ .out=10 });
     nn_net_append(net, (NnLayerParams){ .out=1 });
+
+    //
 
     nn_net_init(net);
 
     net_randomize(net);
 
-    float x[][4 * 2] = {
+    float x[][BATCH_SIZE * 2] = {
         {
             0.0f, 0.0f,
             0.0f, 1.0f,
@@ -40,7 +60,7 @@ int main(void) {
         }
     };
 
-    float t[][4 * 1] = {
+    float t[][BATCH_SIZE * 1] = {
         {
             0,
             1,
@@ -49,18 +69,29 @@ int main(void) {
         } 
     };
 
-    const int iter = 100;
+    printf("********************\n");
+    printf("Start learning\n");
+    printf("********************\n");
+
+    const int iter = 1000;
     for (int i = 0; i < iter; i++) {
-        float loss = nn_train_step(net, x[0], t[0], 0.01);
+        float loss = nn_train_step(net, x[0], t[0], 0.5);
         if ((i + 1) % (iter / 10) == 0) {
             printf("[%4d] loss=%f\n", (i + 1), loss);
         }
     }
 
+    printf("********************\n");
+    printf("Answer/predict\n");
+    printf("********************\n");
     float *y = nn_net_forward(net, x[0]);
-    for (int i = 0; i < 4; i++) {
-        printf("%f (%d)\n", y[i], (y[i] > 0.5f ? 1 : 0));
+    for (int i = 0; i < BATCH_SIZE; i++) {
+        printf("%d/%d (%f)\n", (int)t[0][i], get_class(y[i]), y[i]);
     }
+
+    printf("********************\n");
+    printf("Accuracy=%f\n", get_accuracy(y, t[0], BATCH_SIZE));
+    printf("********************\n");
 
     nn_net_free(&net);
 
